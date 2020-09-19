bf90a7ddc9c3643fa4b359554baa1340de2fec4f
solitude93 <1643829905@qq.com>
2020-09-20 02:37:43 +0800

    Site updated: 2020-09-20 02:37:42

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
